b979129ffb7829f84d378b0883b7624375c99332
MarkedOlive <olivdev4@gmail.com>
2022-07-13 18:58:19 -0400

    Homework 7 finished

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -5,8 +5,25 @@ function quickSort(array) {
   // Implementar el método conocido como quickSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
-  // Tu código:
+  // Tu código: 
+  //  i
+  // [5, 1, 4, 2, 8]
+  //  j
+  if (array.length <= 1) {
+    return array;
+  }
 
+  var pivot = array[0];
+  
+  var left = []; 
+  var right = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) left.push(array[i]);
+    else right.push(array[i]);
+  }
+
+  return quickSort(left).concat(pivot, quickSort(right));
 }
 
 function mergeSort(array) {
@@ -14,7 +31,23 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  const half = array.length / 2
+  if(array.length < 2){
+    return array;
+  }
+  const leftArray = array.splice(0, half)
+  return mergeArrays(mergeSort(leftArray),mergeSort(array))
+  function mergeArrays(leftArray, rightArray) {
+    let ary = []
+    while (leftArray.length && rightArray.length) {
+        if (leftArray[0] < rightArray[0]) {
+            ary.push(leftArray.shift())  
+        } else {
+            ary.push(rightArray.shift())
+        }
+    }
+    return [ ...ary, ...leftArray, ...rightArray ]
+  }
 }
 
 // No modificar nada debajo de esta línea
